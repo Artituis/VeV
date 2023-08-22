fcb7feeeb0d5cbc69ab68a1750610d3dc8095277
Artur S <arturslampert@gmail.com>
2023-08-22 13:40:43 +0000

    Criação de mais testes

--- demo/src/test/java/com/example/BarcaTest.java
+++ demo/src/test/java/com/example/BarcaTest.java
@@ -22,4 +22,66 @@ public class BarcaTest {
         Assertions.assertEquals(1, barca.ocupaLugar("F01A01"));
     }
     
+    @Test
+    public void testeAssentoValidoA1B1(){
+        Assertions.assertEquals(3, barca.ocupaLugar("F01A01"));
+    }
+    
+    @Test
+    public void testeAssentoValidoA1B2(){
+        Assertions.assertEquals(0, barca.ocupaLugar("F01A00"));
+    }
+    
+    @Test
+    public void testeAssentoValidoA1B3(){
+        Assertions.assertEquals(0, barca.ocupaLugar("F01A21"));
+    }
+    
+    @Test
+    public void testeAssentoValidoA2B1(){
+        Assertions.assertEquals(0, barca.ocupaLugar("F00A01"));
+    }
+
+    @Test
+    public void testeAssentoValidoA2B2(){
+        Assertions.assertEquals(0, barca.ocupaLugar("F00A00"));
+    }
+
+    @Test
+    public void testeAssentoValidoA2B3(){
+        Assertions.assertEquals(0, barca.ocupaLugar("F00A21"));
+    }
+
+    @Test
+    public void testeAssentoValidoA3B1(){
+        Assertions.assertEquals(0, barca.ocupaLugar("F61A01"));
+    }
+
+    @Test
+    public void testeAssentoValidoA3B2(){
+        Assertions.assertEquals(0, barca.ocupaLugar("F61A00"));
+    }
+
+    @Test
+    public void testeAssentoValidoA3B3(){
+        Assertions.assertEquals(0, barca.ocupaLugar("F61A21"));
+    }
+
+    @Test 
+    public void testeDistribuicaoPesoA1B1(){
+        Assertions.assertEquals(3, barca.ocupaLugar("F01A01"));
+    }
+
+    @Test 
+    public void testeDistribuicaoPesoA1B2(){
+        int quantidade = 0;
+        for(int assento = 1; assento<=20; assento++){
+            for(int fila=5; fila<=10; fila++){
+                quantidade++;
+                barca.ocupaLugarSemVerificacao(fila, assento);
+            }
+        }
+        System.out.println(quantidade);
+        //Assertions.assertEquals(2, barca.ocupaLugar("F01A01"));
+    }
 }
